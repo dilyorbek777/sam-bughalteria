51f78468928e6cf65573d5aac266de27b8071cb4
devdilyorbek <dilyorbekdev@gmail.com>
2024-03-04 11:06:27 +0500

    sdsf

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,20 +1,24 @@
-let form = document.getElementById("signup")
-
+let form = document.getElementById("contact")
+console.log("connected contactjs");
 let req = {}
+
 
 form.addEventListener("change", (e) => {
     req[e.target.name] = e.target.value
     console.log(req);
+
 })
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const data = fetchData('register', req)
-    console.log(data);
-})
-// https://sam-accountant.onrender.com/api/register
+    let data = contact("contact-us", req)
 
-const fetchData = async (url, options) => {
+    console.log(data);
+
+})
+
+const contact = async (url, options) => {
     let res = await fetch(`https://sam-accountant.onrender.com/api/${url}/`, {
         method: "POST",
         body: JSON.stringify(options),
@@ -22,6 +26,6 @@ const fetchData = async (url, options) => {
             'Content-type': 'application/json; charset=UTF-8',
         },
     }).then(d => d.json())
-    console.log(res);
+
     return res
 }
--- assets/js/log.js
+++ assets/js/log.js
@@ -1,4 +1,4 @@
-let form = document.getElementById("signup")
+let form = document.getElementById("signin")
 
 let req = {}
 
@@ -9,10 +9,26 @@ form.addEventListener("change", (e) => {
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const data = fetchData('register', req)
+    const data = fetchData('login', req)
+    let s = JSON.stringify(data)
+    localStorage.setItem("userdata", s)
     console.log(data);
 })
 // https://sam-accountant.onrender.com/api/register
+
+
+let data = localStorage.getItem("user-data")
+if (data === null) {
+    console.log("null");
+}
+else {
+    let dtst = JSON.parse(data)
+    console.log(dtst);
+    if (dtst.token) {
+        location.assign("/dashboard")
+    }
+}
+
 
 const fetchData = async (url, options) => {
     let res = await fetch(`https://sam-accountant.onrender.com/api/${url}/`, {
@@ -22,6 +38,14 @@ const fetchData = async (url, options) => {
             'Content-type': 'application/json; charset=UTF-8',
         },
     }).then(d => d.json())
+    if (res.error) {
+        alert("Xatolik 404. Keyinroq urinib ko'ring")
+
+    }
     console.log(res);
+
+
+    localStorage.setItem("user-data", JSON.stringify(res))
+
     return res
 }
